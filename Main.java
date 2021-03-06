095930ada5ed4bfd84f1a7166a0ce7d738e079ad
ergunjon <ergunjon@gmail.com>
2021-03-06 10:26:58 -0700

    My second commit

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package com.collection;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 4; i++) {
             System.out.println("Hello Batch 8");
             Thread.sleep(2000);
         }
